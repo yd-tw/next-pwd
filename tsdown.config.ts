a2ef3fa43bf9b140e64ea23a10804519fb3e1975
yd-tw <yd960528@gmail.com>
2026-02-28 17:08:51 +0800

    feat: update tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
+  format: ["es"],
   loader: {
     ".html": "text",
   },
-  clean: true,
 });
